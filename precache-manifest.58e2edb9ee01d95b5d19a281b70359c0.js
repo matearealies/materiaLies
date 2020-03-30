c5277e098494856721825b08ba06c31b0a04b3c0
Quang Le (contractor) <quang.le@macys.com>
2020-03-30 05:52:26 -0400

    Updates

---
+++ precache-manifest.58e2edb9ee01d95b5d19a281b70359c0.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91595405666abce4ee07781d84ea1ee7",
+    "url": "/materiaLies/index.html"
+  },
+  {
+    "revision": "127bcf40f9984f001f1a",
+    "url": "/materiaLies/static/css/7.4707e12a.chunk.css"
+  },
+  {
+    "revision": "4605187e47073f000842",
+    "url": "/materiaLies/static/css/9.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "913bedc7bc3b40b876ac",
+    "url": "/materiaLies/static/js/0.106d6406.chunk.js"
+  },
+  {
+    "revision": "9b4c4e93f4bd29da7d87",
+    "url": "/materiaLies/static/js/1.fa46a2a6.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/materiaLies/static/js/1.fa46a2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2524cd4574f6f55acc9",
+    "url": "/materiaLies/static/js/10.63815943.chunk.js"
+  },
+  {
+    "revision": "6b93fbfada2faa16c433",
+    "url": "/materiaLies/static/js/11.9deb6d7e.chunk.js"
+  },
+  {
+    "revision": "834ac6d994492c3029e4",
+    "url": "/materiaLies/static/js/12.053366fc.chunk.js"
+  },
+  {
+    "revision": "b2d6d5efb60215d45614",
+    "url": "/materiaLies/static/js/13.f48e86f6.chunk.js"
+  },
+  {
+    "revision": "d62505967c9432ebb6a7",
+    "url": "/materiaLies/static/js/14.63d7af34.chunk.js"
+  },
+  {
+    "revision": "925f8d4d170f3afd2656",
+    "url": "/materiaLies/static/js/15.b380e9b6.chunk.js"
+  },
+  {
+    "revision": "85746305d6d9b74c58fc",
+    "url": "/materiaLies/static/js/16.a2d07521.chunk.js"
+  },
+  {
+    "revision": "b02e72eff9ebd6d32c8c",
+    "url": "/materiaLies/static/js/17.3e1f0878.chunk.js"
+  },
+  {
+    "revision": "617d56322ac357ffddc39efc351c8546",
+    "url": "/materiaLies/static/js/17.3e1f0878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e703f2048f59e09236",
+    "url": "/materiaLies/static/js/18.96d4ee0c.chunk.js"
+  },
+  {
+    "revision": "0e39808469f737224063",
+    "url": "/materiaLies/static/js/19.943b1734.chunk.js"
+  },
+  {
+    "revision": "a9a1f4fa7880cad8f18b",
+    "url": "/materiaLies/static/js/20.f7e45e30.chunk.js"
+  },
+  {
+    "revision": "1ff62864f9398f3813bd",
+    "url": "/materiaLies/static/js/4.95ac8e08.chunk.js"
+  },
+  {
+    "revision": "f01b6ee26155721017cc5fc8eb2b8875",
+    "url": "/materiaLies/static/js/4.95ac8e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2facef5a69eba562479",
+    "url": "/materiaLies/static/js/5.fa74ecc7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/materiaLies/static/js/5.fa74ecc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f875fc44044c1ffab778",
+    "url": "/materiaLies/static/js/6.07057880.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/materiaLies/static/js/6.07057880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127bcf40f9984f001f1a",
+    "url": "/materiaLies/static/js/7.96b33166.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/materiaLies/static/js/7.96b33166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34edf50ab170d1c8ef8",
+    "url": "/materiaLies/static/js/8.a8425bdd.chunk.js"
+  },
+  {
+    "revision": "4605187e47073f000842",
+    "url": "/materiaLies/static/js/9.64e09baf.chunk.js"
+  },
+  {
+    "revision": "3dcd6f48be52a901f3e4",
+    "url": "/materiaLies/static/js/main.77407e85.chunk.js"
+  },
+  {
+    "revision": "afe68ac06c48d95595975c9c465c41f8",
+    "url": "/materiaLies/static/js/main.77407e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe38c3e543924b417da",
+    "url": "/materiaLies/static/js/runtime-main.b8bfd5cc.js"
+  },
+  {
+    "revision": "2b466f14430eac13337c9452ff05635c",
+    "url": "/materiaLies/static/media/blogPost1.2b466f14.jpg"
+  },
+  {
+    "revision": "f574380de5938344b4dafe04d833270f",
+    "url": "/materiaLies/static/media/blogPost2.f574380d.jpg"
+  },
+  {
+    "revision": "8d115020e50d1cdeeb160b7a2f3a88fb",
+    "url": "/materiaLies/static/media/blogPost3.8d115020.jpg"
+  },
+  {
+    "revision": "29e641ea808c1389ddc7973e7e6b37c8",
+    "url": "/materiaLies/static/media/blogPost4.29e641ea.jpg"
+  },
+  {
+    "revision": "eed51f556c7b96ada363087472035509",
+    "url": "/materiaLies/static/media/blogPost5.eed51f55.jpg"
+  },
+  {
+    "revision": "070efa17c85068a75cf78b4e6874a570",
+    "url": "/materiaLies/static/media/blogPost6.070efa17.jpg"
+  },
+  {
+    "revision": "e69e6026a423d9ec529b57455907b2b1",
+    "url": "/materiaLies/static/media/image1.e69e6026.jpg"
+  },
+  {
+    "revision": "8bb0c1c3f829049e059c5af0d0417f64",
+    "url": "/materiaLies/static/media/image10.8bb0c1c3.jpg"
+  },
+  {
+    "revision": "bda67d264e2b4b06614c31670c6a8948",
+    "url": "/materiaLies/static/media/image3.bda67d26.jpg"
+  },
+  {
+    "revision": "79ebeb1fee81de72e553c9b30632711e",
+    "url": "/materiaLies/static/media/image4.79ebeb1f.jpg"
+  },
+  {
+    "revision": "39a17f69e21b46e0cecbf738282960b6",
+    "url": "/materiaLies/static/media/image5.39a17f69.jpg"
+  },
+  {
+    "revision": "8b868e181cca8220ca5e725a1f7b984d",
+    "url": "/materiaLies/static/media/image6.8b868e18.jpg"
+  },
+  {
+    "revision": "e052cbd6b2db7efc7de674d3047c998c",
+    "url": "/materiaLies/static/media/image7.e052cbd6.jpg"
+  },
+  {
+    "revision": "b23293625f3b70e68020633ce1427566",
+    "url": "/materiaLies/static/media/image8.b2329362.jpg"
+  },
+  {
+    "revision": "b43804d383d9ba29df937e93cca4f1c1",
+    "url": "/materiaLies/static/media/image9.b43804d3.jpg"
+  }
+]);
